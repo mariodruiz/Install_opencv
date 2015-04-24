969a6248844db97ac547b36cfb19287fff3e1b11
Mario Ruiz <mariodruiz@gmail.com>
2015-04-24 22:43:07 +0200

    Script for Install library for raspicam

--- install_opencv_raspberrypi.sh
+++ install_opencv_raspberrypi.sh
@@ -3,32 +3,20 @@
 # Step 0: Again, I’m going to assume that you have just unboxed your Raspberry Pi 2/B+. 
 # Open up a terminal and we’ll start by updating and upgrading installed packages, followed 
 # by updating the Raspberry Pi firmware:
-sudo apt-get update
-sudo apt-get upgrade
-sudo rpi-update   
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 # Step 1: Install the required developer tools and packages.
-sudo apt-get install build-essential cmake pkg-config 
+sudo apt-get install -y rpi-update  build-essential cmake pkg-config libjpeg8-dev libtiff4-dev libjasper-dev \
+libpng12-dev libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libatlas-base-dev gfortran python2.7-dev
 
-# Step 2: Install the necessary image I/O packages. These packages allow you to load various image file 
-# formats such as JPEG, PNG, TIFF, etc.
-sudo apt-get install libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev 
-
-# Step 3: Install the GTK development library. This library is used to build Graphical User Interfaces (GUIs) 
-# and is required for the highgui  library of OpenCV which allows you to view images on your screen.
-sudo apt-get install libgtk2.0-dev 
-
-# Step 4: Install the necessary video I/O packages. These packages are used to load video files using OpenCV.
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev 
-
-# Step 5: Install libraries that are used to optimize various operations within OpenCV.
-sudo apt-get install libatlas-base-dev gfortran 
-
-# Step 6: install pip.
+# Step 2: install pip.
 wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py 						
+sudo python get-pip.py 	
 
-# Step 7: Install  virtualenv  and virtualenvwrapper.
+pip install numpy	
+
+# Step 3: Install  virtualenv  and virtualenvwrapper.
 sudo pip install virtualenv virtualenvwrapper
 # virtualenv and virtualenvwrapper
 export WORKON_HOME=$HOME/.virtualenvs
@@ -36,11 +24,7 @@ source /usr/local/bin/virtualenvwrapper.sh
 source ~/.profile
 mkvirtualenv cv
 
-# Step 8: Now we can install the Python 2.7 development tools.
-sudo apt-get install python2.7-dev
-pip install numpy
-
-#Step 9: Download OpenCV and unpack it.
+#Step 4: Download OpenCV and unpack it.
 wget -O opencv-2.4.10.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.10/opencv-2.4.10.zip/download
 unzip opencv-2.4.10.zip
 cd opencv-2.4.10
@@ -52,13 +36,13 @@ cmake \
 -D BUILD_NEW_PYTHON_SUPPORT=ON \
 -D INSTALL_C_EXAMPLES=ON \
 -D INSTALL_PYTHON_EXAMPLES=ON  \
--D BUILD_EXAMPLES=ON .. /
+-D BUILD_EXAMPLES=ON .. 
 # compile OpenCV
 make -j($shell nproc)
 sudo make install
 sudo ldconfig
 
-# Step 10: symbolic links.
+# Step 5: symbolic links.
 cd ~/.virtualenvs/cv/lib/python2.7/site-packages/
 ln -s /usr/local/lib/python2.7/site-packages/cv2.so cv2.so
 ln -s /usr/local/lib/python2.7/site-packages/cv.py cv.py
